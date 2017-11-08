4800451a492eeb9e1b8b509f103958d60a466260
sanjayyyy <sanjay.ganesan.raj@gmail.com>
2017-11-08 09:16:41 -0800

    Midi Test

--- testing code/testingmidi/testingmidi.ino
+++ testing code/testingmidi/testingmidi.ino
@@ -41,6 +41,7 @@ void loop() {
 
 // plays a MIDI note. Doesn't check to see that cmd is greater than 127, or that
 // data values are less than 127:
+// j
 void noteOn(int cmd, int pitch, int velocity) {
   Serial.write(cmd);
   Serial.write(pitch);
